9f12b5e382e47c9b4d098cf2f9d6c0de9f93eb9b
stwind <stwindfy@gmail.com>
2013-08-22 10:04:19 +0800

    Add sinon.js

--- test/main.js
+++ test/main.js
@@ -1,17 +1,20 @@
 require.config({
   paths: {
-    chai: 'bower_components/chai/chai'
+    chai: 'bower_components/chai/chai',
+    sinon: 'bower_components/sinon/index'
   },
   shim: {
+    sinon: { exports: 'sinon' }
   }
 });
 
-require(['chai'], function (chai) {
+require(['chai', 'sinon'], function (chai, sinon) {
   'use strict';
 
   mocha.setup('bdd');
 
   window.expect = chai.expect
+  window.sinon = sinon;
 
   require([
     'spec/test'
